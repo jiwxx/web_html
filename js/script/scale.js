62442b99671740ea11deeb814e33d93d523252e8
jiwoo <137027636+jiwxx@users.noreply.github.com>
2023-07-28 14:38:10 +0900

    Add files via upload

---
+++ js/script/scale.js
@@ -0,0 +1,13 @@
+let height = 0
+let weight = 0
+let answer = 0
+const userHeight = document.querySelector('#height')
+const userWeight = document.querySelector('#weight')
+const normal_w = document.querySelector('#answer')
+const btn_h = document.querySelector('#btn_h')
+const btn_w = document.querySelector('#btn_w')
+console.log(userHeight,userWeight,normal_w,btn_h,btn_w)
+
+userHeight.addEventListener('click',function(){
+    
+})
