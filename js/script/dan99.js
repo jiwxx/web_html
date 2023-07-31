a83fcc6c10229ce9dd18c301ecdbe9beaa0b472a
jiwoo <137027636+jiwxx@users.noreply.github.com>
2023-08-01 00:04:58 +0900

    Add files via upload

--- js/script/dan99.js
+++ js/script/dan99.js
@@ -1,14 +1,32 @@
-const two = 2;
-let zero = 0;
-let total = 0;
-const number = document.querySelector('.number')
-const answer = document.querySelector('.answer')
-const gugudan = document.querySelector('.gugu')
-const btn = document.querySelector('button')
-console.log (number,answer)
+// const two = 2;
+// let zero = 0;
+// let total = 0;
+// const number1 = document.querySelector('#number1')
+// const number2 = document.querySelector('#number2')
+// const btn = document.querySelector('#btn')
+// const answer = document.querySelector('.answer')
+// console.log (number1,number2,btn,answer)
 
-btn.addEventListener('click',function(){
-    total = two * zero
-    console.log(total)
-    text.innerHTML=
+// btn.addEventListener('click',function(){
+//     total = two * zero
+//     console.log(total)
+//     number2 = zero;
+//     answer.innerHTML=`${total}`
+// })
+const dan2 = document.querySelector('#dan2') //2고정 인풋
+const calc = document.querySelector('#calc')
+const result = document.querySelector('#result')
+const test = document.querySelector('#test')
+console.log (dan2, calc, result)
+console.log (typeof(dan2.value), typeof(calc.value))
+console.log (`2*2=${'2'*'2'}, 2+2=${'2'+'2'}`)
+//연산자 좌우 피연산자가 문자데이터일 경우
+//더하기연산자 (+) 라면 문자+문자= 문자로 연결결과로 보여준다.
+//나머지 연산자(-,*,/,%) >> 문자를 암시적형 변환으로 자동으로
+//숫자 데이터로 변경해서 문자*문자=숫자 결과로 보여준다.
+test.addEventListener('click',function(){
+    let first = Number(dan2.value)
+    let last = Number(calc.value)
+    console.log(first,last,first*last)
+    result.value = first * last
 })
--- js/script/scale.js
+++ js/script/scale.js
@@ -1,16 +1,20 @@
-let height = 0;
-let weight = 0;
+// let height = 0;
+// let weight = 0;
 let total = 0;
-const m = 100;
-const x = 0.9;
 const userHeight = document.querySelector('#height')
 const userWeight = document.querySelector('#weight')
 const normal_w = document.querySelector('#btn')
 const text = document.querySelector('.answer')
 console.log(userHeight,userWeight,normal_w,text)
-
+//결과 버튼 클릭시
+//1. 사용자가 입력한 키와 몸무게 정보 받아오기
 normal_w.addEventListener('click',function(){
-    total = height + weight - m * x;
-    console.log (total)
-    text.innerHTML=`적정체중은 ${total}kg 이며 ${weight-total}kg 초과되셨습니다.`
+    // total = height + weight - m * x;
+    // console.log (total)
+    // text.innerHTML=`적정체중은 ${total}kg 이며 ${weight-total}kg 초과되셨습니다.`
+    let height = Number(userHeight.value) //string -> number
+    let weight = Number(userWeight.value)
+    total = (height - 100) * 0.9;
+    console.log (height,weight,total)
+    text.innerHTML = `적정체중은 ${total}kg 이며 ${weight-total}kg 초과되었습니다.`
 })
